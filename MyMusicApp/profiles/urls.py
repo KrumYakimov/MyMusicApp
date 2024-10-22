172a198ccc4556b0bb7050d0aa69dc79017f9d6a
Krum Yakimov <krum.yakimov@gmail.com>
2024-10-22 16:35:29 +0300

    feat: implement logic for profile page - view details and delete actions

--- MyMusicApp/profiles/urls.py
+++ MyMusicApp/profiles/urls.py
@@ -1,1 +1,8 @@
-# urlpatterns = []
+from django.urls import include, path
+
+from MyMusicApp.profiles.views import ProfileDetailView, ProfileDeleteView
+
+urlpatterns = [
+    path("details/", ProfileDetailView.as_view(), name="profile-details"),
+    path("delete/", ProfileDeleteView.as_view(), name="profile-delete")
+]
--- MyMusicApp/profiles/views.py
+++ MyMusicApp/profiles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, DeleteView
 
-# Create your views here.
+from MyMusicApp.utils.profile_helpers import get_profile
+
+
+class ProfileDetailView(DetailView):
+    template_name = "profiles/profile-details.html"
+
+    def get_object(self, queryset=None):
+        return get_profile()
+
+
+class ProfileDeleteView(DeleteView):
+    template_name = "profiles/profile-delete.html"
+    success_url = reverse_lazy("index")
+
+    def get_object(self, queryset=None):
+        return get_profile()
